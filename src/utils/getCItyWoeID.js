16575931f4b369270e1f51b00d1e44effd8e660e
magdielG <mcdielgmz@hotmail.com>
2021-04-19 20:14:21 -0700

    Get Woeid by City Name from API

--- src/utils/getCItyWoeID.js
+++ src/utils/getCItyWoeID.js
@@ -1,20 +1,19 @@
 const getCityWoeID = async (woeid) => {
-    const corAPI = `https://cors-anywhere.herokuapp.com/`;
-    const API = `https://www.metaweather.com/api/location/${woeid}`;
-    try {
-      const urlAPI = `${corAPI}${API}`;
-      const res = await fetch(urlAPI, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = res.json();
-      return data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  
-  export default getCityWoeID;
-  
+  const corAPI = `https://cors-anywhere.herokuapp.com/`;
+  const API = `https://www.metaweather.com/api/location/${woeid}`;
+  try {
+    const urlAPI = `${corAPI}${API}`;
+    const res = await fetch(urlAPI, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default getCityWoeID;
